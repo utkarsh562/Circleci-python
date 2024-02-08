752417564aa7421e008d366afdbc713f3f5793fa
Utkarsh Pathak <115210677+utkarsh562@users.noreply.github.com>
2024-02-09 01:10:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def say_hello(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    name = 'Utkarsh Pathak'
+    name = 'King Utkarsh Pathak'
     say_hello(name)
     up = to_upper(name)
     print(up)
